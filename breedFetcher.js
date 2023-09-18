04ba57b6ff14ed6a702e6de65cc0e64581d3a74c
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-09-18 15:46:27 -0600

    Adding error handling, and added error message to user if no breedname is entered as command line argument

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,31 +1,35 @@
 const request = require('request');
 
-// Define the API endpoint URL with the query parameter for the Siberian breed
-const apiUrl = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian';
+// Get the breed name from command-line arguments
+const breedName = process.argv[2];
+
+// Check if the user provided a breed name
+if (!breedName) {
+  console.error('Please provide a breed name as a command-line argument.');
+  process.exit(1);
+}
+
+// Define the API endpoint URL with the query parameter for the specified breed
+const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 // Make a GET request to the API endpoint
 request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
+    console.error('Request error:', error);
   } else if (response.statusCode !== 200) {
     console.error(`Received non-200 status code: ${response.statusCode}`);
   } else {
-    //CHeck to see type of body, it will be a string
-    console.log(typeof body);
-    // The 'body' variable contains the response as a string, so parse it to a JavaScript object
+    // Parse the response body into a JavaScript object
     const data = JSON.parse(body);
 
-    // Check the type of the parsed data (should be an array of breed objects)
-    console.log(typeof data);
-
-    // Now you can work with 'data' as a JavaScript object
-    console.log(data);
-
-    // Access specific properties, for example, the first breed's name
     if (data.length > 0) {
-      console.log('Breed Name:', data[0].name);
+      // Breed found, print breed information
+      const breedInfo = data[0];
+      console.log(`${breedInfo.name}: ${breedInfo.description}`);
     } else {
+      // Breed not found
       console.log('Breed not found.');
     }
   }
 });
+
